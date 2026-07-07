d41dac437e245c5a1bd158904c5f7362a92859f2
Axel DG <alexander18br17@gmail.com>
2026-07-07 18:49:56 -0500

    primer_comit

---
+++ android/app/src/main/kotlin/com/jumpup/app/jumpup_idiomas_movil/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jumpup.app.jumpup_idiomas_movil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppConfig {
+  static String get baseUrl =>
+      dotenv.env['API_BASE_URL'] ?? 'http://10.0.2.2:8000/api';
+
+  static const String appName = 'JumpUp';
+
+  static const String appVersion = '1.0.0';
+
+  static const String company = 'Universidad UTE';
+}
